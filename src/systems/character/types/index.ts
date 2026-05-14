3224929784655b187953f0fd1b23ff5976e88943
Artur Mauricio <arturmauricioss@outlook.com>
2026-05-14 00:27:09 -0300

    Correção de Build

--- src/systems/character/types/index.ts
+++ src/systems/character/types/index.ts
@@ -2,6 +2,9 @@ import type { Name } from '@systems/names'
 import type { Race } from '@systems/race'
 
 export interface Character {
+  id: string
   name: Name
   race: Race
+  gender?: string
+  createdAt?: string
 }
